6194bfb9b0ef8ca89ee4d014c3484967fea5caa7
Yuliia Shevchuk <shevchuuchkaaa@gmail.com>
2024-12-22 15:34:48 +0100

    make build

---
+++ index.05d06807.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth/2-t.clientWidth/2,l=e.clientHeight/2-t.clientHeight/2;t.style.left="".concat(c,"px"),t.style.top="".concat(l,"px");
+//# sourceMappingURL=index.05d06807.js.map
